99c1574f2b94e104ecbf6c17024cae8c865a5fa1
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-09-24 14:25:39 +0300

    Added DTOs(ChatMessageDTO, ChatRoomDTO) to BLL

---
+++ BLL/DTOs/ChatMessageDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace BLL.DTOs
+{
+    public class ChatMessageDTO
+    {
+        public Guid ID { get; set; }
+        public string Content { get; set; }
+        public string? NickName { get; set; }
+        public DateTime SendTime { get; set; }
+        public Guid ChatRoomID { get; set; }
+
+        public ChatRoomDTO? ChatRoom { get; set; }
+
+    }
+}
---
+++ BLL/DTOs/ChatRoomDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BLL.DTOs
+{
+    public class ChatRoomDTO
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<ChatMessageDTO>? Messages { get; set; }
+    }
+}
