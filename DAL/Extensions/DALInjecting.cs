50ec86c099ce741d6b8f9362870203b765b48ee5
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-09-24 12:12:21 +0300

    Added DBInitializing and DALInjecting to PL

---
+++ DAL/Extensions/DALInjecting.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL.Extensions
+{
+    public static class DALInjecting
+    {
+        public static void InjectDAL(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<NetmailChatDatabaseContext>(options =>
+            {
+                options.UseSqlServer(configuration["ConnectionString"]);
+                options.EnableSensitiveDataLogging();
+            });
+        }
+    }
+}
